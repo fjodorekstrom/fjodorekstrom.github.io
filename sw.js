63f4c0e5ec49ae919dc62f905615694c33038896
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2015-02-25 21:10:28 +0100

    fixup! Implement service workers

--- sw.js
+++ sw.js
@@ -10,4 +10,37 @@ var urlsToCache = [
 // Set the callback for the install step
 self.addEventListener('install', function(event) {
     // Perform install steps
+    event.waitUntil(CACHE_NAME)
+        .then(function(cache){
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            if(response) {
+                return response;
+            }
+            var fetchRequest = event.request.clone();
+
+
+            return fetch(fetchRequest)
+                .then(function(response){
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var clonedResponse = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache){
+                            cache.put(event.request, clonedResponse);
+                        });
+                    return response;
+                });
+        })
+    );
 });
