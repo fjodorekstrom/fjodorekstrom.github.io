44def4d66bffd8d20aeba317ab9c18851e856c45
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2015-02-25 20:43:18 +0100

    fixup! Implement service workers

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 'use strict';
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/scripts/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ',    registration.scope);
     }).catch(function(err) {
