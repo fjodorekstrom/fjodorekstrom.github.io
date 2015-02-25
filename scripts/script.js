090a72b536137ad31eff62d63746586c9acc04b9
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2015-02-25 20:25:43 +0100

    fixup! Implement service workers

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 'use strict';
 var worker = new Worker('scripts/sw.js');
 worker.postMessage();
-importScripts('./serviceworker-cache-polyfill.js');
+importScripts('scripts/serviceworker-cache-polyfill.js');
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('scripts/sw.js').then(function(registration) {
